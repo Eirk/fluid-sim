22f166a18bf438af1f949b61ef21a66273ed32f1
Erik <paulsonerik0@gmail.com>
2022-11-15 23:17:40 -0500

    -fixed array sizes

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <GL/glut.h>
 #include "fast_fluid_dynamics.h"
 
 static fast_fluid_solver_t fluid_solver;
 
-static float dens_prev_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float dens_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float u_prev_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float u_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float v_prev_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float v_buf[(N_sim+1) * (N_sim+1)] = {0};
+static float dens_prev_buf[(N_sim+2) * (N_sim+2)] = {0};
+static float dens_buf[(N_sim+2) * (N_sim+2)] = {0};
+static float u_prev_buf[(N_sim+2) * (N_sim+2)] = {0};
+static float u_buf[(N_sim+2) * (N_sim+1)] = {0};
+static float v_prev_buf[(N_sim+2) * (N_sim+2)] = {0};
+static float v_buf[(N_sim+2) * (N_sim+2)] = {0};
 
 float visc = 0.5;
 float diff = 0.5;
 
 float dt = 1.0f / 100.0f; // 100Hz 
 
-static float src_dens_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float src_u_buf[(N_sim+1) * (N_sim+1)] = {0};
-static float src_v_buf[(N_sim+1) * (N_sim+1)] = {0};
+static float src_dens_buf[(N_sim+2) * (N_sim+2)] = {0};
+static float src_u_buf[(N_sim+2) * (N_sim+2)] = {0};
+static float src_v_buf[(N_sim+2) * (N_sim+2)] = {0};
 
 int main(void) {
 
