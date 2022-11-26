5d0a9a0930f3374f7dec517e74dd1f1cb6a6dbc7
Erik <paulsonerik0@gmail.com>
2022-11-26 01:00:30 -0500

    - testing drawing pixels

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "SDL/include/SDL.h"
 #include "fast_fluid_dynamics.h"
 
@@ -20,6 +21,29 @@ static float src_dens_buf[(N_sim+2) * (N_sim+2)] = {0};
 static float src_u_buf[(N_sim+2) * (N_sim+2)] = {0};
 static float src_v_buf[(N_sim+2) * (N_sim+2)] = {0};
 
+static void draw_rectangle(SDL_Surface* surface, int x, int y, int width, int height)
+{
+    SDL_LockSurface(surface);
+    // std::vector<uint8_t> pixels(surface->h * surface->pitch, 0);
+    int size_pixels = surface->h * surface->pitch;
+    uint8_t *pixels = malloc(size_pixels * sizeof(uint8_t));
+    memset(pixels, 0, size_pixels * sizeof(uint8_t));
+
+    int dy, dx;
+    int maxwidth = width * 3;
+    for (dy = y; dy < height; dy++) {
+        for (dx = x; dx < maxwidth; dx += 3) {
+            pixels[dx + (dy * surface->pitch)] = 0;
+            pixels[dx + (dy * surface->pitch) + 1] = 255;
+            pixels[dx + (dy * surface->pitch) + 2] = 0;
+        }
+    }
+    memcpy(surface->pixels, pixels, size_pixels);
+    free(pixels);
+
+    SDL_UnlockSurface(surface);
+}
+
 int main(int argc, char *argv[]) {
 
     // fast_fluid_solver_init(&fluid_solver, N_sim, dens_prev_buf, dens_buf, u_prev_buf, u_buf, v_prev_buf, v_buf, visc, diff, dt);
@@ -27,7 +51,7 @@ int main(int argc, char *argv[]) {
     // // create density source
     // src_dens_buf[IX(N_sim / 2, N_sim / 2)] = 1.0f;
 
-    // //create velocty field
+    // //create velocity field
     // for(int i = 1; i < N_sim; i++) {
     //     for(int j = 1; j < N_sim; j++) {
     //         src_u_buf[IX(i,j)] = -0.5f;
